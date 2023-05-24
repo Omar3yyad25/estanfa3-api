ee239f875910735c0b09424ad7770d395b7ecc82
Omar W. Ayyad <s-omar.ayyad@zewailcity.edu.eg>
2023-05-25 02:04:05 +0300

    sadas

--- app/utils/authchecker.js
+++ app/utils/authchecker.js
@@ -2,14 +2,16 @@ const decode = require('./jwtdecode')
 
 function basic(req, res, next) {
     const session = req.cookies['bezkoder-session']  // get sellerId from cookies
-
+    console.log(session)
     if (!session){
+        console.log("no session")
         return res.redirect("http://estanfa3.com/login.html")
     }
 
     const sellerId = decode.jwtdecode(session);
 
     if (!sellerId){
+        console.log("no sellerId")
         return res.redirect("http://estanfa3.com/login.html")
     }
 
