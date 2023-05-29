6caa529690beaef52ee4a4415c707fa4c7544af2
Omar W. Ayyad <s-omar.ayyad@zewailcity.edu.eg>
2023-05-29 19:05:31 +0300

    get

--- app/routes/contract.routes.js
+++ app/routes/contract.routes.js
@@ -11,7 +11,7 @@ module.exports = function(app){
       });    
 
 // Create a new product
-app.post("/createcontract/:productId",  authchecker.basic,contractController.create);
+app.get("/createcontract/:productId",  authchecker.basic,contractController.create);
 
 app.get("/getcontracts/", authchecker.basic, contractController.getContractsById);
 
