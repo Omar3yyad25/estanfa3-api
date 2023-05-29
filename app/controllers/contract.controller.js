6fe049f7a88285e124f2ae439d837378c55bbbbb
Omar W. Ayyad <s-omar.ayyad@zewailcity.edu.eg>
2023-05-29 18:55:40 +0300

    .

--- app/controllers/contract.controller.js
+++ app/controllers/contract.controller.js
@@ -5,12 +5,14 @@ const url = require('url');
 
 // Create a new contract
 exports.create = async (req, res) => {
+
   const { productId } = req.params;
   console.log(productId)
 
   const session = req.cookies['bezkoder-session']  // get sellerId from cookies
   const buyerId = decode.jwtdecode(session);
   const queries = req.params
+  console.log(queries)
 
   if (!productId){
     console.log("no product id", productId)
