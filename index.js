01760ae9f240dee88ccca9ad40a857b579ea9d12
Omar W. Ayyad <s-omar.ayyad@zewailcity.edu.eg>
2023-05-29 17:45:41 +0300

    route

--- index.js
+++ index.js
@@ -107,6 +107,7 @@ require("./app/routes/auth.routes")(app);
 require("./app/routes/user.routes")(app);
 require("./app/routes/products.routes")(app);
 require("./app/routes/offer.routes")(app);
+require("./app/routes/contract.routes")(app);
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8443;
